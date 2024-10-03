2d8a24f6e4b11e0f721467e9c86c6e810d28d39b
Koshal <80113133+K0shal@users.noreply.github.com>
2024-10-03 16:01:59 +0530

    Create us_states.sql

---
+++ us_states.sql
@@ -0,0 +1,63 @@
+DROP TABLE IF EXISTS states;
+
+-- Create the table with statename, latitude, and longitude
+CREATE TABLE states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    statename VARCHAR(50) NOT NULL,
+    latitude DECIMAL(9, 6),
+    longitude DECIMAL(9, 6)
+);
+
+-- Insert the 50 U.S. states with placeholder latitude and longitude values
+INSERT INTO states (statename, latitude, longitude)
+VALUES
+('Alabama', 32.318230, -86.902298),
+('Alaska', 64.200841, -149.493673),
+('Arizona', 34.048928, -111.093731),
+('Arkansas', 35.201050, -91.831833),
+('California', 36.778259, -119.417931),
+('Colorado', 39.550051, -105.782067),
+('Connecticut', 41.603221, -73.087749),
+('Delaware', 38.910832, -75.527670),
+('Florida', 27.994402, -81.760254),
+('Georgia', 32.157435, -82.907123),
+('Hawaii', 19.896766, -155.582782),
+('Idaho', 44.068202, -114.742041),
+('Illinois', 40.633125, -89.398529),
+('Indiana', 40.267194, -86.134902),
+('Iowa', 41.878003, -93.097702),
+('Kansas', 39.011902, -98.484246),
+('Kentucky', 37.839333, -84.270018),
+('Louisiana', 30.984298, -91.962333),
+('Maine', 45.253783, -69.445469),
+('Maryland', 39.045755, -76.641271),
+('Massachusetts', 42.407211, -71.382437),
+('Michigan', 44.314844, -85.602364),
+('Minnesota', 46.729553, -94.685900),
+('Mississippi', 32.354668, -89.398528),
+('Missouri', 37.964253, -91.831833),
+('Montana', 46.879682, -110.362566),
+('Nebraska', 41.492537, -99.901813),
+('Nevada', 38.802610, -116.419389),
+('New Hampshire', 43.193852, -71.572395),
+('New Jersey', 40.058324, -74.405661),
+('New Mexico', 34.519940, -105.870090),
+('New York', 43.299428, -74.217933),
+('North Carolina', 35.759573, -79.019300),
+('North Dakota', 47.551493, -101.002012),
+('Ohio', 40.417287, -82.907123),
+('Oklahoma', 35.007752, -97.092877),
+('Oregon', 43.804133, -120.554201),
+('Pennsylvania', 41.203322, -77.194525),
+('Rhode Island', 41.580095, -71.477429),
+('South Carolina', 33.836081, -81.163725),
+('South Dakota', 43.969515, -99.901813),
+('Tennessee', 35.517491, -86.580447),
+('Texas', 31.968599, -99.901813),
+('Utah', 39.320980, -111.093731),
+('Vermont', 44.558803, -72.577841),
+('Virginia', 37.431573, -78.656894),
+('Washington', 47.751074, -120.740139),
+('West Virginia', 38.597626, -80.454903),
+('Wisconsin', 43.784440, -88.787868),
+('Wyoming', 43.075968, -107.290284);
